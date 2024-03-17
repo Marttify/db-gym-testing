80a5ef78b1d5f37f6600b5b2bd9235b2f84bff8f
Martin Aguilar <aguilarmartin977@gmail.com>
2024-03-17 03:09:57 -0300

    feat: request for update of the put type through the body

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { Pets, User } from './database.js';
 
+import bodyParser from 'body-parser'
 const app = express();
 const port = process.env.PORT || 3000; // Set the port
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -45,8 +49,7 @@ app.get('/get-pets/:id', async (req, res) => {
 
 // Actualizar: 
 // /update-user/:id para actualizar el usuario, que se pueda actualizar el nombre, y que el nombre se pase por el body en un tipo de request PUT
-// /update-pet/:id para actualizar el mascota, que se pueda actualizar el nombre, y que el nombre se pase por el body en un tipo de request PUT
-
+// /update-user/:id para actualizar el usuario, a travez del body se pueda actualizar el nombre, el email y la fecha de nacimiento
 
 app.put('/update-user/:id', async (req, res) => {
   const id = req.params.id;
@@ -55,11 +58,7 @@ app.put('/update-user/:id', async (req, res) => {
     res.status(404).send('Usuario no encontrado.');
     return;
   }
-  await user.update({ name: "Pedrooooooo" }, {
-    where: {
-      name: null
-    }
-  });
+  await user.update({ name: "Ema" });
   res.send('Usuario actualizado');
 });
 
@@ -67,23 +66,44 @@ app.put('/update-pet/:id', async (req, res) => {
   const id = req.params.id;
   let pets = await Pets.findByPk(id);
   if (!pets) {
-    res.status(404).send('Usuario no encontrado.');
+    res.status(404).send('Mascota no encontrado.');
     return;
   }
-  await pets.update({ name: "Patoooooooo" }, {
-    where: {
-      name: null
-    }
-  });
+  // body
+  await pets.update({ name: "Akira" });
   res.send('Mascota actualizada');
 });
 
+// Actualizar: 
+// /update-pet/:id para actualizar el mascota, que se pueda actualizar el nombre, y que el nombre se pase por el body en un tipo de request PUT
+// /update-pet/:id para actualizar el mascota, a travez del body se pueda actualizar el nombre, la raza y la fecha de nacimiento
+app.put('/update-user/:id', async (req, res) => {
+  const id = req.params.id;
+  let user = await User.findByPk(id);
+  if (!user) {
+    res.status(404).send('Usuario no encontrado.');
+    return;
+  }
+  await user.update({ name: req.body.name });
+  res.send('Usuario actualizado');
+});
+
+app.put('/update-pet/:id', async (req, res) => {
+  const id = req.params.id;
+  let pets = await Pets.findByPk(id);
+  if (!pets) {
+    res.status(404).send('Mascota no encontrado.');
+    return;
+  }
+  // body
+  await pets.update({ name: req.body.name });
+  res.send('Mascota actualizada');
+});
 
 
 // Eliminar
 // /delete-user/:id para eliminar el usuario con el id que le estamos pasando
 // /delete-pet/:id para eliminar la mascota con el id que le estamos pasando
-
 app.delete('/delete-user/:id', async (req, res) => {
   const id = req.params.id;
   let user = await User.findByPk(id);
@@ -111,4 +131,6 @@ app.delete('/delete-pet/:id', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
 
